c30d26c99e7ea25044dc9bed52dbfaaaf7c18d75
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2023-10-19 15:36:40 -0300

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace PropproAssistant;
+
+public partial class App : Application
+{
+    private Window m_window;
+
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        m_window = new MainWindow();
+        m_window.Activate();
+    }
+}
