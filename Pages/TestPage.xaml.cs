ada8b2e19af5da2c7a6bd95d930c34e77c198261
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2023-11-10 16:53:22 -0300

    style: basic DataGrid implemented

--- Pages/TestPage.xaml.cs
+++ Pages/TestPage.xaml.cs
@@ -1,11 +1,29 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.UI.Xaml.Controls;
+using PropproAssistant.Data;
+using PropproAssistant.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace PropproAssistant.Pages;
 
 public sealed partial class TestPage : Page
 {
+    public ObservableCollection<BiddingItem> Items { get; }
+    public double PageHeight;
+
     public TestPage()
     {
         InitializeComponent();
+
+        using var db = new AppDbContext();
+        int id = 3;
+        var bidding = db.Biddings.Include(b => b.Items).FirstOrDefault(x => x.Id == id);
+
+        if (bidding != null)
+        {
+            Items = new ObservableCollection<BiddingItem>(bidding.Items);
+        }
+        DataGrid.ItemsSource = Items;
     }
 }
